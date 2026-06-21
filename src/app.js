2551b5936866cc37f74ceaff17bc599d5a36df27
Vishakha Patil <patilvishakha2303@gmail.com>
2026-06-21 19:56:17 +0100

    Updated testing API

--- src/app.js
+++ src/app.js
@@ -8,10 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// //  test
-// app.get('/', (req, res) => {
-//   res.json({ message: 'CDR Analytics API is running' });
-// });
+//  test
+app.get('/', (req, res) => {
+  res.json({ message: 'CDR Analytics API is running' });
+});
 app.use('/api/cdr', cdrRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
